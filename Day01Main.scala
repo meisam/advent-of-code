6151e59007360d7818fc6dec4c0a4a96ba6c7231
Meisam FS <meisam.fathi@gmail.com>
2025-03-16 06:50:49 +0000

    [day01] Run the input to solution for part 2

--- Day01Main.scala
+++ Day01Main.scala
@@ -3,7 +3,13 @@ package day01
 @main
 def main: Unit =
   scala.util.Using(scala.io.Source.fromFile("day01.input")): source =>
-    day01.parse(source.mkString) match
+    val input = source.mkString
+    day01.parse(input) match
       case scala.util.Success(s)  => println(f"OUTPUT: $s")
       case scala.util.Failure(ex) => println(f"Error: $ex")
-      case err =>        println(f"Error: $err")
+      case err                    => println(f"Error: $err")
+
+    day01.similarityScore(input) match
+      case scala.util.Success(s)  => println(f"OUTPUT: $s")
+      case scala.util.Failure(ex) => println(f"Error: $ex")
+      case err                    => println(f"Error: $err")
